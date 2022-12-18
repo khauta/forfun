c989ebd2eda8d57ca3e055421d2ac3ef6fda42d9
khauta <kjamaliehe@outlook.com>
2022-12-18 05:57:09 +0200

    committia

--- app.js
+++ app.js
@@ -1,41 +1,28 @@
-var express = require('express');
-var cors = require('cors');
-const uRt = require('./api/sms/sms.router');
+const router = require('./lib/core/index.cjs.js');
 
+// set vars for login
 
-// creating the app instance
-var app = express();
-//app.set('view engine', 'ejs');   for setting node tempplate engine
+router.config.setUrl('http://192.168.8.1');
+router.config.setUsername('admin');
+router.config.setPassword('Technomath');
 
+async function myAsync(data)  {
+    const loggedIn = await router.admin.isLoggedIn();
+          if (!loggedIn) {
+            // If we aren't, login
+            await router.admin.login();
+          }
 
-// cors addition
-app.use(cors());
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+          // crazy test duo
 
-// setting express to use json
-app.use(express.json());
-
-// just to veryfiy server is running
-app.get('/', (req, res) => {
-  res.json({ 'message': 'ok' });
-})
-
-//handing over user related route to the their router module
-app.post('/', (req, res) => {
-    console.log(req.query);
-    res.json({ 'message': `${req.body}` });
-  })
-
-//handing over user related route to the their router module
-app.post('/api/sms', uRt)
-
-
-app.listen(2020);
-console.log('Listening on port 2020');
+          // pass the random # to the content
+          // data.content = data.content + `${Math.floor(100000 + Math.random() * 900000)}`;
+          let parsedData = data;
+          
+          const stats = await router.ussd.sendUssdCommand(parsedData); //getMobileNetworkInfo();//
+          const stats2 = await router.ussd.getUssdStatus();
+          console.log(stats2);
+          return stats;
+};
+// particular use case for sanity check [preliminary test]
+myAsync("*111*100#").then(val => console.log(val));
